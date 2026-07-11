4f38ca4c16c653324872b1e15fdc49afe5cbe849
youzi177 <youzi177@126.com>
2026-07-11 18:43:41 +0800

    集成邮箱接口

--- src/config/MailConfig.js
+++ src/config/MailConfig.js
@@ -6,8 +6,8 @@ async function send(sendInfo) {
     port: 465, //一般是465 SSL
     secure: true, //465端口需要修改为true  use STARTTLS (upgrade connection to TLS after connecting)
     auth: {
-      user: 'youzi177@126.com',
-      pass: 'AFeUnadT7jAJGbGY',
+      user: 'XXXX126.com',
+      pass: 'XXX',
     },
   });
   // const sendInfo = {
@@ -27,7 +27,7 @@ async function send(sendInfo) {
   const url = 'https://github.com/youzi177';
   //发送邮件
   const info = await transporter.sendMail({
-    from: '"Geek极客社区" <youzi177@126.com>', // sender address
+    from: '"Geek极客社区" <XXXX126.com>', // sender address
     to: sendInfo.email, // list of recipients
     subject: subject, // 邮件标题
     text: `您在Geek极客社区的验证码是${sendInfo.code},验证码过期时间:${sendInfo.expire}`, // plain text body
