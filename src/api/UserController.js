f2e28e9ea5b306271a940473393c70effd6f9772
youzi177 <youzi177@126.com>
2026-07-30 16:48:02 +0800

    签到逻辑优化

--- src/api/UserController.js
+++ src/api/UserController.js
@@ -10,15 +10,16 @@ class UserController {
     // 1：取用户ID
     const obj = await getJWTpPayload(ctx.header.authorization)
     // 2：查询用户上一次的签到记录
-    const record = await SignModel.findById(obj._id)
+    // record 根据用户 id 查询这个用户最近一次的签到记录
+    const record = await SignModel.findByUid(obj._id)// 查uid
     const user = await UserModel.findByID(obj._id)
     // 3:判断签到逻辑
     if (record !== null) {
-      // 有历史签到记录
-      // 判断用户上次签到的时间是否与今天相同
-      // 如果当前时间的日期与用户上一次签到的日期相同，说明用户已经签到
-      const count = user.count// 用户签到的天数
+      // 3.1有历史签到记录
+      let count = user.count// 用户签到的天数
       let fav = 0
+      // 3.3判断用户上次签到的时间是否与今天相同
+      // 如果当前时间的日期与用户上一次签到的日期相同，说明用户已经签到
       if (moment(record.created).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')) {
         ctx.body = {
           code: 500,
@@ -26,11 +27,12 @@ class UserController {
           count: user.count,
           msg: '用户已经签到'
         }
+        return
       } else {
-        // 有上一次的签到记录，并且不与今天相同，进行连续签到的判断
-
+        // 3.4有上一次的签到记录，并且不与今天相同，进行连续签到的判断
         // 判断签到时间,用户上一次签到的时间是当前时间的前一天，说明在连续签到
-        if (moment(record.lastSign).format('YYYY-MM-DD') === moment().subtract(1, 'days').format('YYYY-MM-DD')) {
+        if (moment(record.created).format('YYYY-MM-DD') === moment().subtract(1, 'days').format('YYYY-MM-DD')) {
+          count += 1// 临界情况，比如签到第4天，再次签到就是第5天，第五天应该是10积分
           // 连续签到的积分获得逻辑
           if (count < 5) {
             fav = 5
@@ -52,9 +54,10 @@ class UserController {
               $inc: { favs: fav, count: 1 }
             }
           )
+          // 返回数据
           result = {
-            favs: user.favs + fav,
-            count: user.count + 1
+            favs: user.favs + fav, // 返回总积分
+            count: user.count + 1 // 因为user.count是旧数据所以是+1
           }
         } else {
           // 用户中断了连续签到
@@ -63,41 +66,43 @@ class UserController {
             { _id: obj._id },
             {
               $set: { count: 1 }, // 修改设置成1
-              $int: { favs: fav }// 增加
+              $inc: { favs: fav }// 增加
             }
           )
-          // 更新用户的签到记录
-          newRecord = new SignModel({
-            uid: obj._id,
-            favs: fav,
-            lastSign: record.created
-          })
-          await newRecord.save()
+          // 返回数据
           result = {
             favs: user.favs + fav,
             count: 1
           }
         }
+        // 无论是否连续签到，更新用户的签到记录
+        newRecord = new SignModel({
+          uid: obj._id,
+          favs: fav,
+
+        })
+        await newRecord.save()
       }
     } else {
-      // 也就是注册之后第一次签到
+      // 3.2无历史签到数据
       // 无签到数据,那就写入数据，count签到修改为1天，favs增加5
       await UserModel.updateOne({
         _id: obj._id
       }, {
         $set: { count: 1 }, // 修改设置成1
-        $int: { favs: 5 }// 增加
+        $inc: { favs: 5 }// 增加
       })
       // 保存用户的签到记录
       newRecord = new SignModel({
         uid: obj._id,
         favs: 5,
-        lastSign: moment().format('YYYY-MM-DD HH:mm:ss')
+
       })
       await newRecord.save()
+      // 返回的签到数据
       result = {
-        favs: 5,
-        count: 1
+        favs: user.favs + 5, // 返回总积分
+        count: 1// 签到天数为1
       }
     }
     ctx.body = {
--- src/model/Sign.js
+++ src/model/Sign.js
@@ -1,19 +1,22 @@
 import mongoose from '../config/DBHelpler'
-import moment from 'moment'
+// import moment from 'moment'
 const Schema = mongoose.Schema
 const SignSchema = new Schema({
   uid: { type: String, ref: 'users' },
   created: { type: Date },
   favs: { type: Number },
-  lastSign: { type: Date }
+  // lastSign: { type: Date }
 })
 
 SignSchema.pre('save', function () {
-  this.created = moment().format('YYYY-MM-DD HH:mm:ss')
+  // this.created = moment().format('YYYY-MM-DD HH:mm:ss')
+  this.created = new Date()
 })
-//
-SignSchema.static = {
-  findbyuid: function (uid) {
+/**
+ * 根据用户 id 查询这个用户最近一次的签到记录
+ */
+SignSchema.statics = {
+  findByUid: function (uid) {
     return this.findOne({ uid }).sort({ created: -1 })
   }
 }
