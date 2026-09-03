3df134a2fcd914fdea7b902131741e0a1839b237
youzi177 <youzi177@126.com>
2026-09-03 20:23:31 +0800

    fix sort bug-1

--- src/api/UserController.js
+++ src/api/UserController.js
@@ -376,7 +376,7 @@ class UserController {
     const page = params.page ? parseInt(params.page) : 0
     const limit = params.limit ? parseInt(params.limit) : 0
     const sort = params.sort ? params.sort : 'created'
-    const result = await UserModel.getList({}, page, limit, sort)
+    const result = await UserModel.getList({}, sort, page, limit)
     const total = await UserModel.countList({})
     ctx.body = {
       code: 200,
